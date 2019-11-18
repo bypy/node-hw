e5e8dfea5595407b9eb82dfd1354af9183cd6ab1
Дудко Алексей Алексеевич <alexey.dudko@service-it.by>
2019-11-18 18:44:48 +0300

    Debugging script added

--- 3097_Validation_form/index.js
+++ 3097_Validation_form/index.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const webserver = express();
 const port = 3000;
-let current = new Date().getFullYear();
-let age = current - 2014;
-let phrase = '';
 
 webserver.get('/validate', (req, res) => {
-    if (age<2) {
-        phrase = '<h1>Буба мне люба!</h1>';
-        age++;
-        current++;
-    } else 
-        phrase = `<h1>Буба мне ${age++} года люба!</h1>
-                 <p>Это будет в ${current++} году</p>`;
-    for (var i in req) {
-        if (i === 'protocol')
-            console.log (req[i]);
-    }
-    res.send(phrase);
+    let responseData = '';
+    res.send(responseData);
 });
 
 webserver.listen(port);
