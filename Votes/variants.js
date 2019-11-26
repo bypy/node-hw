d8cdc2fca7fa4a80b979663db8d646fcce8ca929
Alexey <js.dudko@gmail.com>
2019-11-26 23:07:29 +0300

    Мок-сервис для получения вариантов ответов

--- Votes/variants.js
+++ Votes/variants.js
@@ -1,59 +1,58 @@
 'use strict';
-function reqListener () {
-    console.log(this.responseText);
-}
-  
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://localhost:8080/variants");
-oReq.send();
+// SoapUI-5.5.0.vmoptions
+// -Dfile.encoding=UTF8
+let formWrapper = document.querySelector('.vote-wrapper');
+let variantsURL = 'http://localhost:8089/variants';
 
+let performRequest = function(url) { 
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (xhr.status === 200)
+                resolve(xhr.responseText);
+            else
+                reject(xhr.statusText);
+        };
+        xhr.onerror = function() {
+            reject(url);
+        };
+        xhr.open('GET', url);
+        xhr.send();   
+    });
+};
 
+let createInputs = questions => {
+    let inputs = questions.map(q => 
+        `<label class="gaidai"><input type="radio" name="gaidai" value="${q.code}">${q.text}</label>`
+    );
+    return inputs;
+};
 
-// let inputGroup = document.querySelector('.inputs');
-// let variantsURL = 'http://localhost:8080/variants';
+let createForm = inputs => {
+    let formMarkup = 
+        `<form id="vote-form" action="#" onsubmit="sendAndUpdate()">
+            ${inputs}
+        </form>`;
+    return formMarkup;
+    
+};
 
-// let performRequest = function(url) { 
-//     return new Promise(function(resolve, reject) {
-//         let xhr = new XMLHttpRequest();
-//         xhr.onload = function() {
-//           if (xhr.status === 200)
-//             resolve(xhr.responseText);
-//           else
-//             reject(xhr.statusText)
-//         };
-//         xhr.onerror = function() {
-//             reject(xhr);
-//         };
-//         xhr.open('GET', url);
-//         xhr.send();   
-//     });
-// };
+let createErrorResponse = e => {
+    console.log(`Ошибка ${e}`);
+};
 
-// let parseResponse = res =>
-//     JSON.parse(res)
-
-// let createInputs = questions => {
-//     let inputs = questions.map(q => 
-//         `<input type="radio" name="${q.code}" value="${q.value}" class="gaidai">`
-//     );
-//     appendInputs(inputs.join('\n'));
-// };
-
-// let appendInputs = inputs => {
-//     inputGroup.innerHTML = inputs;
-// }
-
-// let getVariants = performRequest(variantsURL);
-// getVariants
-//     .then(res =>
-//         JSON.parse(res)
-//     )
-//     .then( questions => {
-//         createInputs(questions);
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     })
-//     ;
+let getVariants = performRequest(variantsURL);
+getVariants
+    .then(res =>
+        JSON.parse(res)
+    )
+    .then( questions => createInputs(questions) )
+    .then( inputs => createForm(inputs.join('\n')) )
+    .then( formMarkup => {
+        formWrapper.innerHTML = formMarkup;
+    })
+    .catch(function(e) {
+        createErrorResponse(e);
+    })
+;
         
