a3f73ae109d32b36df526c8fa1882c478b9ef188
Дудко Алексей Алексеевич <alexey.dudko@service-it.by>
2019-12-06 13:42:48 +0300

    Переименовал каталог со статикой, вынес подготовку данных "опросника" в отдельный модуль

--- Votes_level2/app/app.js
+++ Votes_level2/app/app.js
@@ -1,74 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const iconv = require('iconv-lite');
-const os = require('os');
+
+// для импорта путей к файлам-источникам:
+const dataSources = require('./prepareQuestion.js');
+// название ключевого поля в хэше вопросов
+const keyName = require('./keyName.js')
 
 const webserver = express();
 const servPort = 7980;
 
 webserver.use(
-    '/public',
-    express.static(path.join(__dirname,'..','static'))
+    '/voting',
+    express.static(path.join(__dirname,'..','public'))
 );
 
 webserver.use(express.urlencoded({extended:true}));
 
-
-const variantsSrcFilePath = path.join(__dirname, 'вопросы для опроса.txt');
-const variantsTargetFilePath = path.join(__dirname, 'variants.json');
-const statTargetFilePath = path.join(__dirname, 'stat.json');
-
-// название ключевого поля в хэше вопросов
-const keyName = 'code';
-let jsonFileBody;
-
-// специалист по опросам набирал текст вопросов в Akelpad
-// и загрузил документ 'вопросы для опроса.txt' в кодировке windows-1251 
-let buf = fs.readFileSync(variantsSrcFilePath);
-let fileContent = iconv.decode(buf, 'win1251');
-let questions = fileContent.split(os.EOL);
-// формирую содержимое JSON файла с вариантами ответов
-let index = 0;
-let questionList = questions.map(qText => {
-    index++;
-    let questionH = {};
-    questionH[keyName] = 'q'+ index.toString();
-    questionH.text = qText;
-    return questionH;
-});
-jsonFileBody = iconv.encode(JSON.stringify(questionList), 'utf8');
-// записал в файл
-fs.writeFileSync(variantsTargetFilePath, jsonFileBody);
-
-
-// инициализация содержимого JSON файла со статистикой ответов
-let statRec = {};
-questionList.forEach(q => {
-    let code = q.code;
-    if (code) statRec[code] = 0;
-});
-jsonFileBody = JSON.stringify(statRec);
-fs.writeFileSync(statTargetFilePath, jsonFileBody);
-
-
-
-
-// webserver.get('/mainpage/voting.html', (req, res) => {
-//     try {
-//         res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-//         res.send(mainPage);
-//     } catch(e) {
-//         res.status(404).end();
-//     }
-// });
-
-// webserver.get('/pic.jpg', (req, res) => {
-//     const filePath=path.join(__dirname, 'pic.jpg');
-//     const fileStream=fs.createReadStream(filePath);
-//     res.setHeader('Content-Type', 'image/jpeg');
-//     fileStream.pipe(res);
-// });
+// пути к файлам-источникам текста вопросов и статистики ответов
+const variantsTargetFilePath = dataSources.variants;
+const statTargetFilePath = dataSources.stat;
 
 webserver.get('/variants', (req, res) => {
     res.setHeader('Content-Type', 'application/json; charset=UTF-8');
---
+++ Votes_level2/app/prepareQuestion.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const iconv = require('iconv-lite');
+// название ключевого поля в хэше вопросов
+const keyName = require('./keyName.js')
+
+const variantsSrcPath = path.join(__dirname, 'вопросы для опроса.txt');
+const variantsTargetPath = path.join(__dirname, 'variants.json');
+const statTargetPath = path.join(__dirname, 'stat.json');
+
+// const keyName = 'code';
+let jsonFileBody;
+
+// специалист по опросам набирал текст вопросов в Akelpad
+// и загрузил документ 'вопросы для опроса.txt' в кодировке windows-1251 
+let buf = fs.readFileSync(variantsSrcPath);
+let fileContent = iconv.decode(buf, 'win1251');
+let questions = fileContent.split(os.EOL);
+// формирую содержимое JSON файла с вариантами ответов
+let index = 0;
+let questionList = questions.map(qText => {
+    index++;
+    let questionH = {};
+    questionH[keyName] = 'q'+ index.toString();
+    questionH.text = qText;
+    return questionH;
+});
+jsonFileBody = iconv.encode(JSON.stringify(questionList), 'utf8');
+// записал в файл
+fs.writeFileSync(variantsTargetPath, jsonFileBody);
+
+
+// инициализация содержимого JSON файла со статистикой ответов
+let statRec = {};
+questionList.forEach(q => {
+    let code = q.code;
+    if (code) statRec[code] = 0;
+});
+jsonFileBody = JSON.stringify(statRec);
+fs.writeFileSync(statTargetPath, jsonFileBody);
+
+
+module.exports = {
+    variants: variantsTargetPath,
+    stat: statTargetPath
+};
