f494131b18781837fa6ddcf4e9fb66c467d293df
Alexey <js.dudko@gmail.com>
2019-12-08 20:03:30 +0300

    Настроено кэширование для /stat и /export

--- Votes_level2/app/app.js
+++ Votes_level2/app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const fsp = require('fs').promises;
 const path = require('path');
+const sha256 = require('js-sha256').sha256;
 //const { logLineAsync, getTempFileName } = require('../../utils/utils');
 
 // для импорта путей к файлам-источникам:
@@ -25,7 +26,9 @@ const topic = dataSources.votingTopic;
 webserver.get('/variants', (req, res) => {
     res.setHeader('Content-Type', 'application/json; charset=UTF-8');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(fs.readFileSync(variantsTargetFilePath, 'utf8'));
+    const fileStream = fs.createReadStream(variantsTargetFilePath);
+    fileStream.pipe(res);
+    //res.send(fs.readFileSync(variantsTargetFilePath, 'utf8'));
 });
 
 webserver.post('/vote', (req, res) => {
@@ -41,30 +44,37 @@ webserver.post('/vote', (req, res) => {
 });
 
 webserver.get('/stat', async (req, res) => {
-    if (Object.keys(req.query).length === 0) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'application/json; charset=UTF-8');
-        res.send(fs.readFileSync(statTargetFilePath, 'utf8'));
+    let statData = fs.readFileSync(statTargetFilePath, 'utf8');
+    let hash = sha256(statData);
+    let ifNoneMatch = req.header('If-None-Match');
+    if ( ifNoneMatch && (ifNoneMatch === hash) ) {
+        res.status(304).end();
     } else {
-        // запрос на статистику в определенном формате
-        const clientAccept = req.headers.accept;
-        const requestedFormat = clientAccept.split('/')[1].trim();
-        console.log(requestedFormat);
+        res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+        res.setHeader('ETag', hash);
+        res.setHeader('Cache-Control', 'public, max-age=0');
+        res.send(statData);
+    }  
+});
 
-        switch (requestedFormat) {
-        case 'html':
-            res.setHeader('Content-Type', 'text/html; encoding=UTF-8');
-            break;
-        case 'xml':
-            res.setHeader('Content-Type', 'application/xml; encoding=UTF-8');
-            break;
-        case 'json':
-            res.setHeader('Content-Type', 'application/json; encoding=UTF-8');
-            break;
-        default:
-            res.status(204).send('Экспорт невозможен');
-            return;
-        }
+webserver.get('/export', async (req, res) => {
+    const clientAccept = req.headers.accept;
+    const requestedFormat = clientAccept.split('/')[1].trim();
+    switch (requestedFormat) {
+    case 'html':
+        res.setHeader('Content-Type', 'text/html; encoding=UTF-8');
+        break;
+    case 'xml':
+        res.setHeader('Content-Type', 'application/xml; encoding=UTF-8');
+        break;
+    case 'json':
+        res.setHeader('Content-Type', 'application/json; encoding=UTF-8');
+        break;
+    default:
+        res.status(204).send('Экспорт невозможен');
+        return;
+    }
+    try {
         const variantsH = JSON.parse(await fsp.readFile(variantsTargetFilePath));
         const statH = JSON.parse(await fsp.readFile(statTargetFilePath));
         const combinedStatH = ((questions, stat) => {
@@ -74,70 +84,83 @@ webserver.get('/stat', async (req, res) => {
             });
             return questions;
         })(variantsH, statH);
-        res.send(await transformStatFormat(requestedFormat, combinedStatH));
-    }
-    function transformStatFormat(targetFormat, statList) {
-        let questionNodes,
-            transformedStat;
-        if (targetFormat === 'xml') {
-            questionNodes = statList.map( q => `
-                <question>
-                    <text>${q.text}</text>
-                    <votes>${q.votes}</votes>
-                </question>`
-            );
-            transformedStat = 
-                `<votingStat>
-                    <votingTopic>${topic}</votingTopic>
-                    <questionsList>${questionNodes.join('')}
-                    </questionsList>           
-                </votingStat>`;
-
-        } else if (targetFormat === 'html') {
-            questionNodes = statList.map( q => `
-                <tr>
-                    <td>${q.text}</td>
-                    <td>${q.votes}</td>
-                </tr>`
-            );
-            transformedStat = 
-                `<!DOCTYPE html>
-                <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Статистика голосования</title>
-                </head>
-                <body>
-                    <table>
-                        <caption>Статистика голосования "${topic}"</caption>
-                        <thead>
-                            <tr>
-                                <th>Текст вопроса</th>
-                                <th>Число голосов</th>
-                            </tr>
-                        </thead>
-                        <tbody>${questionNodes.join('')}
-                        </tbody>
-                    </table>	
-                </body>
-                </html>`;
-        } else if (targetFormat === 'json') {
-            questionNodes = statList.map( q => `
-        {
-            "text":"${q.text.replace(/"/gm, '\\"')}", 
-            "votes":${q.votes}
-        }`);
-            
-            transformedStat = 
-`{
-    "votingTopic": "${topic}",
-    "stat": [${questionNodes.join(',')}
-    ]
-}`;
-
+        let statData = transformStatFormat(requestedFormat, combinedStatH);
+        let hash = sha256(statData);
+        let ifNoneMatch = req.header('If-None-Match');
+        if ( ifNoneMatch && (ifNoneMatch === hash) ) {
+            res.status(304).end();
+        } else {
+            console.log('New ETag='+hash);
+            res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+            res.setHeader('ETag', hash);
+            res.setHeader('Cache-Control', 'public, max-age=0');
+            res.send(statData);
         }
-        return transformedStat;
+    } catch (err) {
+        res.status(204).send('Экспорт невозможен');
     }
 });
+
+function transformStatFormat(targetFormat, statList) {
+    let questionNodes,
+        transformedStat;
+    if (targetFormat === 'xml') {
+        questionNodes = statList.map( q => `
+            <question>
+                <text>${q.text}</text>
+                <votes>${q.votes}</votes>
+            </question>`
+        );
+        transformedStat = 
+            `<votingStat>
+                <votingTopic>${topic}</votingTopic>
+                <questionsList>${questionNodes.join('')}
+                </questionsList>           
+            </votingStat>`;
+
+    } else if (targetFormat === 'html') {
+        questionNodes = statList.map( q => `
+            <tr>
+                <td>${q.text}</td>
+                <td>${q.votes}</td>
+            </tr>`
+        );
+        transformedStat = 
+            `<!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Статистика голосования</title>
+            </head>
+            <body>
+                <table>
+                    <caption>Статистика голосования "${topic}"</caption>
+                    <thead>
+                        <tr>
+                            <th>Текст вопроса</th>
+                            <th>Число голосов</th>
+                        </tr>
+                    </thead>
+                    <tbody>${questionNodes.join('')}
+                    </tbody>
+                </table>	
+            </body>
+            </html>`;
+    } else if (targetFormat === 'json') {
+        questionNodes = statList.map( q => `
+            {
+                "text":"${q.text.replace(/"/gm, '\\"')}", 
+                "votes":${q.votes}
+            }`);
+        
+        transformedStat = 
+            `{
+                "votingTopic": "${topic}",
+                "stat": [${questionNodes.join(',')}
+                ]
+            }`;
+    }
+    return transformedStat;
+}
 
 webserver.listen(servPort);
