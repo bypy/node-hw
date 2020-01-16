57160402a9bba645ba4170fa794a7b0fafce2a02
Alexei <mail@mail.by>
2020-01-16 18:24:03 +0300

    Добавил в пайп busboy

--- 5695_FileStorage/app.js
+++ 5695_FileStorage/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-const { logLineAsync } = require('./utils/utils');
+const bodyParser= require('body-parser');
+const fs = require('fs');
+const multer = require('multer');
+const Busboy = require('busboy');
+// const { logLineAsync } = require('./utils/utils');
+
+const port = 7980;
+const upload = multer({ dest: 'uploads/' });
+// const logFN = path.join(__dirname, '_server.log');
 
 const webserver = express();
-const port = 7980;
-const logFN = path.join(__dirname, '_server.log');
-
+webserver.use(bodyParser.urlencoded({extended: true}));
 
 webserver.get('/', async (req, res, next) => { 
     //logLineAsync(logFN,"обращение к / - рендерим как /main");
@@ -16,11 +21,24 @@ webserver.get('/', async (req, res, next) => {
 });
 
 webserver.get('/upload', (req, res) => {
-    fs.createReadStream(path.join(__dirname, 'static', 'upload-form.html'))
-        .pipe(res);
+    // fs.createReadStream(path.join(__dirname, 'static', 'upload-form.html'))
+    //     .pipe(res);
+    res.sendFile(path.join(__dirname, 'static', 'upload-form.html'));
 });
 
+//webserver.post('/upload', upload.single('attach'), (req, res) => {
 webserver.post('/upload', (req, res) => {
+    const busboy = new Busboy({ headers: req.headers  });
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        console.log(fieldname, filename, encoding, mimetype);
+        file.on('data', data => {
+            console.log('got ' + data.length + ' bytes');
+        });
+    });
+    busboy.on('finish', function() {
+        console.log("That's all folks!");
+    });
+    req.pipe(busboy);
     res.send('Спасибо!');
 });
 
