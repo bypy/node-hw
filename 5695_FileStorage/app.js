4c928b08c6657b16f50080703a9b5150a2aeee6f
Alexei <123@mail.by>
2020-01-19 23:43:14 +0300

    File storage со скачиванием

--- 5695_FileStorage/app.js
+++ 5695_FileStorage/app.js
@@ -1,20 +1,118 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const sha256 = require('js-sha256').sha256;
 const Busboy = require('busboy');
 const uuid = require('uuid-v4');
 
 const webserver = express();
 const port = 7980;
-const uploadPath = path.join(__dirname, 'uploads');
+const uploadPath = path.join(__dirname, 'upload');
+const dbPath = path.join(__dirname, 'data');
+const mainPagePath = path.join(__dirname, 'static', 'upload-form.html');
 
-const uploadedFilesDataH = {};
 
-const storeFileInfo = (savedName, dataH) => {
-    uploadedFilesDataH[savedName] = dataH;
-    console.log(JSON.stringify(uploadedFilesDataH));
-    console.log('Saved');
+const sortIdKeys = (prev, next) => {
+    if (parseInt(prev) < parseInt(next)) return 1; else return -1;
+}
+
+
+const restoreFileInfo2Hash = async (idsArr, done) => {
+    idsArr.sort(sortIdKeys).reverse(); //
+    for (let i=0; i<idsArr.length; i++) {
+        let current = path.join(dbPath, idsArr[i]);
+        try {
+            let content = await fs.readFileSync(current);
+            fileInfoHash[i.toString()] = JSON.parse(content);
+        } catch(err) {
+            console.log(err);
+            continue;
+        }
+    }
+    done();
+}
+
+
+const storeFileInfo = data => {
+    // заапустим процесс записи в файловую БД
+    return new Promise( (resolve, reject) => {
+        const fileInfoSavePath = path.join(dbPath, nextSaveId.toString().concat('.json'));
+        const fileInfoContent = JSON.stringify(data);
+        fs.writeFile(fileInfoSavePath, fileInfoContent, (error) => {
+            if (error) {
+                reject(error);
+            }
+            nextSaveId++;
+            // дополнительно запишем в хэш
+            fileInfoHash[nextSaveId.toString()] = data;
+            console.log('Saved')
+            resolve();
+        });
+    });
 };
+
+
+const getDownloadsMarkup = () => {
+    let ids = Object.keys(fileInfoHash);
+    ids.sort(sortIdKeys);
+    let tableRows = ids.map( id => 
+        `<tr><td>${fileInfoHash[id]['origFN']}</td>
+        <td>${fileInfoHash[id]['comment']}</td>
+        <td><a href="/${id.toString()}">Скачать файл<a></td>
+        </tr>`
+    );
+
+    return tableRows.join('\n');
+};
+
+
+const getPageMarkup = downloadsMarkup => {
+    const newMainMarkup = mainPageContent.split('{$}').join(downloadsMarkup);
+    mainPageHash = sha256(newMainMarkup); // актуализируем хэш-сумму главной страницы
+    return newMainMarkup;
+};
+
+
+const getFileInfoById = id => fileInfoHash[id];
+
+
+const startWebServer = () => {
+    webserver.listen(port , () => {
+        console.log('Listening port 7980...');
+    });
+}
+
+const fileInfoHash = {};
+const fileInfoArr = fs.readdirSync(dbPath); // число записей в файловой ДБ
+let nextSaveId = fileInfoArr.length ; // идентификатор для сохранения следующего файла с описанием
+let mainPageContent = fs.readFileSync(mainPagePath, 'utf8');
+let mainPageHash = sha256(mainPageContent); // для проверки возможности ответить 304
+
+
+if (nextSaveId > 0)
+    // восставновление данных о загруженных файлах в хэш в памяти
+    // с запуском слушателя запросов
+    restoreFileInfo2Hash(fileInfoArr, startWebServer);
+else
+    startWebServer();
+
+
+webserver.use(express.static(path.join(__dirname, 'static')));
+
+
+webserver.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    if (/\d/.test(parseInt(id))) {
+        const fileInfo = getFileInfoById(id);
+        const originalName = encodeURIComponent(fileInfo.origFN);
+        // https://stackoverflow.com/questions/7967079/special-characters-in-content-disposition-filename/7969807#7969807
+        res.setHeader("Content-Disposition", `attachment; filename*=UTF-8''${originalName}`);
+        res.sendFile(path.resolve(__dirname,"upload",fileInfo.saveFN));
+    } else {
+        next();
+    }
+});
+
 
 webserver.get('/', async (req, res, next) => { 
     //обращение к / - рендерим как /upload;
@@ -23,58 +121,94 @@ webserver.get('/', async (req, res, next) => {
     next();
 });
 
+
+
+
 webserver.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'upload-form.html'));
+    let ifNoneMatch = req.header('If-None-Match');
+    if ( ifNoneMatch && (ifNoneMatch === mainPageHash) ) {
+        res.status(304).end();
+        console.log('Cached');
+    } else {
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+        res.setHeader('ETag', mainPageHash);
+        res.setHeader('Cache-Control', 'public, max-age=0');
+        try {
+            let downloadsMarkup = getDownloadsMarkup();
+            let pageMarkup = getPageMarkup(downloadsMarkup);
+            res.send(pageMarkup);
+        } catch(err) {
+            console.log(err);
+            res.end(500);
+        };
+    }
 });
 
 webserver.post('/upload', (req, res) => {
     
-    const uploadingFileInfo = {
-        saveFN:null,
-        origFN:null
-    };
-    const COMMENT_FIELD_NAME = 'comment';
+    const uploadFileInfo = {}; // имя файла, временное имя файла для хранения и комментарий к файлу фиксируем здесь
     const contentLength = req.headers['content-length'];
+
     
-    const busboy = new Busboy({ headers: req.headers });
-    req.pipe(busboy);
+    const noFileResponse = () => {
+        // сделать отправку через вебсокет
+        // {"error":"Вы не выбрали файл для загрузки", "data":null}
+        res.send('Вы не выбрали файл для загрузки');
+    };
 
-    busboy.on('field', (fieldname, val) => {
-        if (fieldname === COMMENT_FIELD_NAME) {
-            console.log(fieldname, val);
-            uploadingFileInfo[COMMENT_FIELD_NAME] = val;
-        }
-    });
+    
+    try {
+        const busboy = new Busboy({ headers: req.headers });
+        req.pipe(busboy);
 
-    busboy.on('file', (fieldname, file, filename) => {
-        let fileSize = 0;
-        
-        file.on('data', data => {
-            fileSize += data.length;
-            console.log(`${contentLength - fileSize} bytes remains`);
+        busboy.on('field', (fieldname, val) => {
+            if (fieldname === 'comment') {
+                // 
+                uploadFileInfo[fieldname] = val;
+            }
         });
 
-        let saveFileName = uuid() + path.extname(filename);
-        uploadingFileInfo.saveFN = saveFileName;
-        uploadingFileInfo.origFN = filename;
-        
-        let saveToPath = path.join(uploadPath,saveFileName);
-        file.pipe(fs.createWriteStream(saveToPath));
-    });
+        busboy.on('file', (fieldname, file, filename) => {
+            if (filename === '' || fieldname !== 'attachedFile') {
+                noFileResponse();
+                return;
+            }
+            console.log('Здесь установить сокет-соединение');
+            let fileSize = 0;
+            file.on('data', dataChunk => {
+                fileSize += dataChunk.length;
+                console.log(`${contentLength - fileSize} bytes remains`);
+            });
+            let saveFileName = uuid() + path.extname(filename);
+            uploadFileInfo.saveFN = saveFileName;
+            uploadFileInfo.origFN = filename;
+            let saveToPath = path.join(uploadPath,saveFileName);
+            file.pipe(fs.createWriteStream(saveToPath));
+        });
 
-    busboy.on('finish', function() {
-        console.log('Zero bytes remains');
-        if (uploadingFileInfo.saveFN && uploadingFileInfo.origFN) {// можно загружать файл без комментария
-            console.log('Ready to save in hash');
-            let nameKey = uploadingFileInfo.origFN;
-            delete uploadingFileInfo.origFN;
-            storeFileInfo(nameKey, uploadingFileInfo);
-        }
-    });
-    
-    res.send('Спасибо!');
+        busboy.on('finish', function() {
+            console.log('0 bytes remains');
+            if ( !uploadFileInfo.hasOwnProperty('origFN') ) {
+                // файл обязателен (комментарий нет)
+                noFileResponse();
+                return;
+            } else {
+                console.log('Здесь разорвать сокет-соединение');
+                console.log('Ready to save in hash');
+                storeFileInfo(uploadFileInfo)
+                    .then( () => {
+                        res.redirect(303, '/'); // GET
+                    })
+                    .catch( (err) => {
+                        console.log("Catched error: ");
+                        console.log(err);
+                        res.redirect(303, '/'); // GET
+                    });
+            }
+        });
+
+    } catch(err) {
+        console.log(err);
+        res.end(501);
+    }
 });
-
-webserver.use(express.static(path.join(__dirname, 'static')));
-
-webserver.listen(port);
